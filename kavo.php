e5adb462bccbd3d859d77b8ca52d3e6a14a0bb30
Johan Vervloet <johanv@johanv.org>
2016-11-29 12:54:14 +0100

    Correct url to form.

--- kavo.php
+++ kavo.php
@@ -161,7 +161,8 @@ function kavo_civicrm_summaryActions(&$actions, $contactID) {
     'title' => 'Generate KAVO-ID',
     'ref' => 'generate_kavo_id',
     'key' => 'kavo_id',
-    'href' => CRM_Utils_System::url("/kavo/controller", "action=new_id"),
+    'weight' => 0,
+    'href' => CRM_Utils_System::url("/civicrm/kavo/controller", "action=new_id"),
   ];
 }
 
